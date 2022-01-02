ed2e105fb140c2892ab3795e28b69e5d9f37e92d
Ali Neaz <neazali77@gmail.com>
2022-01-02 16:46:13 +0600

    basic data types if R done

---
+++ pratice_all.R
@@ -0,0 +1,72 @@
+# # print("Hello R programm")
+
+# a <- 50
+# b <- 54
+# sam <- a+b 
+# sam 
+
+# 18-45
+# 16/4
+# 2**3
+# 2^3
+
+# # squart root  
+# 9**.5
+# 9**(1/2)
+
+
+# # raminder  
+# 14%%4
+# 14.34%%4
+# 8%%3
+# # integer division 
+# 8 %/% 3
+# 7 %/% 3
+# 7%%3
+# T+T
+# T+F
+# F+F
+# F+T
+# "Hello"+"R"
+
+
+x <- 45.34
+x 
+class(x)
+
+y <- 23
+y
+class(y)
+typeof(y)
+z <- "Hello"
+z
+class(z)
+typeof(z)
+
+
+a <- as.integer(45.15)
+a
+is.integer(a)
+
+d <-  as.integer(23.4324)
+class(d)
+d
+is.integer(d)
+
+
+# complex numbers 
+comp <- 4+3i
+comp
+class(comp)
+# is.comple(comp)
+
+
+z <- "12.4"
+z
+class(z)
+
+
+b <- as.character(4575)
+b
+class(b)
+typeof(b)
