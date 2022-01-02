9f58010aa6bc1fd0665ab907a6688f75611cda13
Ali Neaz <neazali77@gmail.com>
2022-01-02 20:12:14 +0600

    done till break and next. Next if functions

---
+++ 10_break_next.R
@@ -0,0 +1,16 @@
+# break and next statement ------------------
+# break
+for (i in 1:10) {
+  if (i==5)
+    break
+  print(i)
+}
+
+# printing the odd numbers
+for (i in 1:10){
+  if (i %%2==0){
+    print(i)
+  }else{
+    print(i)
+  }
+}
