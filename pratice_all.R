a4aff649efaccdde11b50b1c726f999f3cf43ff9
Ali Neaz <neazali77@gmail.com>
2022-01-02 17:04:35 +0600

    first practice done till sequence in R

--- pratice_all.R
+++ pratice_all.R
@@ -30,43 +30,161 @@
 # "Hello"+"R"
 
 
-x <- 45.34
-x 
-class(x)
+# x <- 45.34
+# x 
+# class(x)
 
-y <- 23
-y
-class(y)
-typeof(y)
-z <- "Hello"
-z
-class(z)
-typeof(z)
+# y <- 23
+# y
+# class(y)
+# typeof(y)
+# z <- "Hello"
+# z
+# class(z)
+# typeof(z)
 
 
-a <- as.integer(45.15)
+# a <- as.integer(45.15)
+# a
+# is.integer(a)
+
+# d <-  as.integer(23.4324)
+# class(d)
+# d
+# is.integer(d)
+
+
+# # complex numbers 
+# comp <- 4+3i
+# comp
+# class(comp)
+# # is.comple(comp)
+
+
+# z <- "12.4"
+# z
+# class(z)
+
+
+# b <- as.character(4575)
+# b
+# class(b)
+# typeof(b)
+
+#  logical operators 
+# x <- T
+# y <- F
+# x||y
+# x &y
+# !y
+
+# 10 & 1
+# 10 & 0
+# !-10
+
+
+
+# Relational operators 
+
+# a <-19
+# b <- 30
+# a>b
+# a<b
+# a>=b
+# a<=b
+# a=b
+# a==b
+# a
+# b
+# a <-12
+# a==b
+# a
+# b
+
+# a != b
+# b != a
+
+
+# x <-32.42
+# y <- 32.423
+# x >= b
+# x>=y
+# y >= x
+
+
+#  c<- "Hello"
+# d <- "Neo"
+# c>d
+# c<d
+# c <= d
+# x+ y
+
+# sequency 
+ a<- 1:10
 a
-is.integer(a)
+a
+a <- 1:10*2
+a
+b <- 1:100
+b
 
-d <-  as.integer(23.4324)
-class(d)
-d
-is.integer(d)
-
-
-# complex numbers 
-comp <- 4+3i
-comp
-class(comp)
-# is.comple(comp)
+ a <- 10
+b <- 1:a
+b
 
 
-z <- "12.4"
+x <- seq(1,10)
+x
+x <- seq(1,100,2)
+x
+x <- seq(2,100,2)
+x
+
+y <- seq(1,100,length(5))
+y
+y <- seq(1,100,length=5)
+y
+
+a <- seq(1,10,length=100)
+a
+
+# variable assignment
+
+x <-10
+y = 23
+x
+y
+assign("z",45)
 z
+typeof(z)
 class(z)
 
 
-b <- as.character(4575)
+a<-b<-c<-6
+a
 b
-class(b)
-typeof(b)
+c
+x <- pi
+x
+
+y <- letters
+y
+z <- LETTERS
+z
+c <- month.name
+c
+c <- month.abb
+c
+
+
+# variables in r 
+
+my_var = 13
+my_var
+myVar<-"Neaz"
+myVar
+typeof(my_var)
+typeof(myVar)
+
+mode(my_var)
+my_var
