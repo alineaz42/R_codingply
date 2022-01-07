f7476abcd8709c8a343410e5b8b7e01287435d60
Ali Neaz <neazali77@gmail.com>
2022-01-08 02:39:26 +0600

    before matrix functions done need to practice more one this vid: 3:32

--- 22_diag_mat.R
+++ 22_diag_mat.R
@@ -60,3 +60,90 @@ sells
 
 sells[c(1,3),]
 sells[c(1,3),c(1,2)]
+
+
+# rbind and cbind
+
+matA <- matrix(1:9,3,3,byrow=T)
+matA
+rownames(matA)=c("Row1","Row2","Row3")
+colnames(matA)=c("Col1","Col2","Col3")
+matA
+matA[Row1,]
+matA[1,]
+matA[,1]
+
+b <- 10:13
+b
+c <- rbind(matA,b)
+c
+
+
+silk <- c(14,44,74,84,57,14)
+dry <- c(14,44,74,85,57,14)
+normal<- c(14,44,74,85,57,20)
+products <- c(silk,dry,normal)
+sells <- rbind(products)
+sells
+sells <- matrix(sells,5,3,byrow=T)
+sells
+# suppose new product on the market black shine
+blackshine<-c(21,21,34,12,53)
+sells <- cbind(sells,blackshine)
+sells
+colnames(sells)=c("Silk","Dry","Normal","Black Shine")
+sells
+sum(sells[,3])
+sum(sells[,4])
+# suppose entry another months sells into the main sells in Aug
+rownames(sells)=c("Jan","Feb","March","May","Jun")
+sells
+Aug <- c(32,43,23,53)
+sells<-rbind(sells,Aug)
+sells
+
+
+A <- matrix(1:9,3,3,byrow=T)
+A
+B <- matrix(1:3,3,1)
+B
+C <- cbind(A,B)
+C
+D <- matrix(c(4,5,6,7),1,4)
+E <- rbind(C,D)
+E
+dim(E)
+
+
+
+
+# Operations on matrices
+
+A <- matrix(1:9,3,3,byrow=T)
+B <- matrix(10:18,3,3,byrow=T)
+A
+B
+
+A+B
+B-A
+A^2
+# not element wise square
+A %*% A
+A %*% B
+#transpose
+t(A)
+solve(A)
+dim(A)
+solve(B)
+A^(-1)
+
+
+# finding the inverse
+a1 <- c(4,3,6)
+a2 <- c(6,2,3)
+a3 <- c(6,5,8)
+
+A <- rbind(a1,a2,a3)
+print(A)
+# returns the inverse of the matrix
+T1 <- solve(A)
